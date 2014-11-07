27a70aebe0e48d920141a08c7bc7aa195157fb8a
Michael van Bracht <mvb@solute.de>
2014-11-07 10:12:51 +0100

    Depend at least von requests 2.4
    Does not work with 2.0.x, 2.4 definitly works.

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ if __name__ == '__main__':
         scripts=_glob.glob('scripts/tues*'),
         include_package_data=True,
         platforms='any',
-        install_requires=['docopt', 'fabric', 'requests'],
+        install_requires=['docopt', 'fabric', 'requests>=2.4'],
         classifiers=[
             'Programming Language :: Python',
             'Development Status :: 4 - Beta',
