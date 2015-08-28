0839c864fda862fb19757a953fb898c7117b4bec
Michael van Bracht <mvb@solute.de>
2015-08-28 10:25:50 +0200

    packaging: Remove README.txt

--- setup.py
+++ setup.py
@@ -1,24 +1,8 @@
 #!/usr/bin/env python
 # coding: utf-8
-import os as _os
-import io as _io
 import glob as _glob
 
 import setuptools as _st
-
-here = _os.path.abspath(_os.path.dirname(__file__))
-
-def read(*filenames, **kwargs):
-    encoding = kwargs.get('encoding', 'utf-8')
-    sep = kwargs.get('sep', '\n')
-    buf = []
-    for filename in filenames:
-        with _io.open(filename, encoding=encoding) as f:
-            buf.append(f.read())
-    return sep.join(buf)
-
-
-long_description = read('README.txt', 'CHANGES.txt')
 
 
 if __name__ == '__main__':
@@ -30,7 +14,6 @@ if __name__ == '__main__':
         author='Michael van Bracht',
         author_email='michael@wontfix.org',
         description='Easy remote command execution',
-        long_description=long_description,
         packages=_st.find_packages(),
         scripts=_glob.glob('scripts/tues*'),
         include_package_data=True,
