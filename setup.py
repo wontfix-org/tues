897487a486d78a10bf9e931abbe48ee07aaf09ad
Michael van Bracht <mvb@solute.de>
2021-03-19 18:31:00 +0100

    packaging: Consistently use doublequotes

--- setup.py
+++ setup.py
@@ -1,30 +1,30 @@
 #!/usr/bin/env python
 # coding: utf-8
+
 import glob as _glob
 
 import setuptools as _st
 
 import tues as _tues
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     _st.setup(
-        name='tues',
+        name="tues",
         version=_tues.__version__,
-        url='https://github.com/wontfix-org/tues/',
-        license='MIT',
-        author='Michael van Bracht',
-        author_email='michael@wontfix.org',
-        description='Easy remote command execution',
+        url="https://github.com/wontfix-org/tues/",
+        license="MIT",
+        author="Michael van Bracht",
+        author_email="michael@wontfix.org",
+        description="Easy remote command execution",
         packages=_st.find_packages(),
-        scripts=_glob.glob('scripts/tues*'),
+        scripts=_glob.glob("scripts/tues*"),
         include_package_data=True,
-        platforms='any',
+        platforms="any",
         long_description=open("README.md").read(),
         long_description_content_type="text/markdown",
-        install_requires=['click', 'fabric3', 'requests>=2.4'],
+        install_requires=["click", "fabric3", "requests>=2.4"],
         classifiers=[
-            'Programming Language :: Python',
-            'Programming Language :: Python :: 3',
-            'Natural Language :: English',
+            "Programming Language :: Python :: 3",
+            "Natural Language :: English",
         ],
     )
