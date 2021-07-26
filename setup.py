760660cb60d39a3db26e9bac13bfc1d3cb121682
Michael van Bracht <mvb@solute.de>
2021-07-26 14:56:33 +0200

    Version Bump (2.0.0)

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ import setuptools as _st
 if __name__ == "__main__":
     _st.setup(
         name="tues",
-        version="2.0.0rc3",
+        version="2.0.0",
         url="https://github.com/wontfix-org/tues/",
         license="MIT",
         author="Michael van Bracht",
