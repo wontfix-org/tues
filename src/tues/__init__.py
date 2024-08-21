4da2aef3667f8adaab1cf736841a25df828f3adf
Michael van Bracht <michael@wontfix.org>
2024-08-21 15:46:01 +0200

    Guard against negative / None returncode
    The returncode can be negative when exiting from a signal, and it may
    even be `None` when there are some error during communication before
    the execute command can exit properly.
    
    To be able to better debug the source of the `None` values, we first try
    getting rid of any followup exceptions that might shadow the source of
    these problems.

--- src/tues/__init__.py
+++ src/tues/__init__.py
@@ -866,7 +866,7 @@ async def run_tasks_serial(tasks, pm=_PM, stdout=None, stderr=None, check=False)
 
         if task.authorization_failed:
             raise TuesUserAbort("Sudo authorization failed")
-        if exc or (check and result.returncode > 0):
+        if exc or (check and result.returncode != 0):
             raise TuesTaskError(task) from exc
         results.append(result)
 
