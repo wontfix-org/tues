wontfix-org/tues

commit 9f22228e0a19a5e3995ea0d414105d66a8e63b61

Add "class" option to "fm" provider
With the recent Foreman upgrade, the "class" search field has been
renamed to "puppetclass". This is of course massively more typing and
thus needs to be abbreviated.

src/tues/cli/tues_provider_fm.py
@@ -34,8 +34,20 @@ def hosts(url, query=None):
 
 @_click.command()
 @_click.option("-f", "--foreman-url", required=True, envvar="FOREMAN_URL")
-@_click.argument("expression")
-def cli(foreman_url, expression):
+@_click.option("-c", "--class", "class_", help="Select hosts with the given Puppet class. Use * for globbing.")
+@_click.argument("expression", required=False)
+def cli(foreman_url, expression, class_):
+    if class_:
+        op = "~" if "*" in class_ else "="
+        class_query = f"puppetclass {op} \"{class_}\""
+
+        if expression:
+            expression = f"({class_query}) and ({expression})"
+        else:
+            expression = class_query
+    elif not expression:
+        raise _click.ClickException("No query specified")
+
     for host in hosts(foreman_url, expression):
         _click.echo(host)
 

commit 1afff4a652639be7222db033e7f57762096b3e80

Restore support for empty query in Foreman provider
This was broken in 9f22228e0a (Add "class" option to "fm" provider,
2024-12-13). Calling the provider with an empty query is a common
idiom for "get all hosts", so restore support for it.

src/tues/cli/tues_provider_fm.py
@@ -51,7 +51,7 @@ def cli(foreman_url, expression, class_):
             expression = f"({class_query}) and ({expression})"
         else:
             expression = class_query
-    elif not expression:
+    elif expression is None:
         raise _click.ClickException("No query specified")
 
     for host in hosts(foreman_url, expression):